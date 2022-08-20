59d5f78eed42a8956c8f9a6dbf4e75fa40829ce7
Pratham <prathamnagaria@gmail.com>
2022-08-21 02:26:29 +0530

    Time: 196 ms (59.80%), Space: 14.4 MB (10.13%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
@@ -4,12 +4,16 @@ class Solution:
         pq=PriorityQueue()
         max_reach_distance=startFuel
         ind=ans=0
+        #Enter loop only if our maximum reachable distance is less than target
         while max_reach_distance<target:
+        #Now once we enter the loop check for how many stations we can cross/jump with our current fuel capacity
             while ind<len(stations) and max_reach_distance>=stations[ind][0]:
                 pq.put(-stations[ind][1])
                 ind+=1
+            # if we cannot increase our max_reach and also at this stage our max_reach is less than target so return -1
             if pq.empty():
                 return -1
+            #increase our max_reach_distance to the greatest fuel station till now
             max_reach_distance+=(-pq.get())
             ans+=1
         return ans
