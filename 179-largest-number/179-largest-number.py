894141344b0c5776c91d11a6bcd1ed45f0cd7ade
Pratham <prathamnagaria@gmail.com>
2022-08-22 02:36:17 +0530

    Time: 58 ms (65.15%), Space: 13.8 MB (97.78%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,9 +1,21 @@
-class comparator(str):
+"""class comparator(str):
     def __lt__(x,y):
         return x+y>y+x
     
 class Solution:
 	def largestNumber(self,arr):
 	    ans=''.join(sorted(map(str,arr),key=comparator))
-        # to check if the final string contains only zeroes then return just single zero
-	    return '0' if ans[0]=='0' else ans
+	# to check if the final string contains only zeroes then return just single zero
+	    return '0' if ans[0]=='0' else ans"""
+from functools import cmp_to_key
+class Solution:
+    def largestNumber(self,arr):
+        def comparator(x,y):
+            if x+y>y+x:
+                return -1
+            elif x==y:
+                return 0
+            else:
+                return 1
+        ans=''.join(sorted(map(str,arr),key=cmp_to_key(comparator)))
+        return '0' if ans[0]=='0' else ans
