b5f759eb5080757009474517a1ba311adbeaa90a
Pratham <prathamnagaria@gmail.com>
2022-08-22 11:08:25 +0530

    Time: 61 ms (25.23%), Space: 14 MB (10.18%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,8 +1,18 @@
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        b=bin(n)[2:]
-        if n<0:
-            return False
-        if b.count('1')==1 and (len(b)-1)%2==0:
-            return True
-        return False
+    def isPowerOfFour(self, num: int) -> bool:
+        # b=bin(n)[2:]
+        # if n<0:
+        #     return False
+        # if b.count('1')==1 and (len(b)-1)%2==0:
+        #     return True
+        # return False
+        return num > 0 and num & (num-1) == 0 and 0b1010101010101010101010101010101 & num == num  
+    
+    # to check a number is a power of two ( n>0 and n&(n-1)==0)
+    # for power of 4 the only one present must be present at odd position so to check that we & with big number according to constraints
+    """
+16=                           10000
+    1010101010101010101010101010101
+    _______________________________
+    0000000000000000000000000010000  equal to num
+    """
