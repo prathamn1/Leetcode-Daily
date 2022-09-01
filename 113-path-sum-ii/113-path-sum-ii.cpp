f650594a9aeb366f371acb041d393c5f535a3206
Pratham <prathamnagaria@gmail.com>
2022-09-01 17:25:03 +0530

    Time: 16 ms (66.69%), Space: 19.9 MB (81.56%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
 class Solution {
 public:
     void dfs(TreeNode* node,int curr_sum,vector<int>curr_path,vector<vector<int>>&ans) {
@@ -28,6 +30,31 @@ public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>ans;
         dfs(root,targetSum,vector<int>(),ans);
+        return ans;
+    }
+};
+*/
+class Solution {
+public:
+    void dfs(TreeNode* node,int curr_sum,vector<int>&curr_path,vector<vector<int>>&ans) {
+        if(not node) {
+            return;
+        }
+        curr_sum-=node->val;
+        curr_path.push_back(node->val);
+        if(not node->left and not node->right) {
+            if(curr_sum==0) {
+                ans.push_back(curr_path);
+            }
+        }
+        dfs(node->left,curr_sum,curr_path,ans);
+        dfs(node->right,curr_sum,curr_path,ans);
+        curr_path.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>>ans;
+        vector<int>curr_path;
+        dfs(root,targetSum,curr_path,ans);
         return ans;
     }
 };
