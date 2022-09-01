a551cd82ce88b5e77caa42f72d9b852352e1e442
Pratham <prathamnagaria@gmail.com>
2022-09-01 17:31:48 +0530

    Time: 13 ms (76.30%), Space: 20 MB (61.80%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -64,6 +64,7 @@ public:
 
 //we can also implement in a way like when we call dfs we subtract value there and there only and while checking for the valid sum we check in a way like -- suppose we are standing on node x , then we will check that is the curr_sum value is node x->val and also that the node x is leaf node.
 
+//Actually what i was doing in the above two solutions that first I was adding current node value to the curr_sum and then checking if it is the leaf node and curr_sum==0 ,but here in below solution i am first adding the current_node value to my current_path and then checking that if my curr_sum==current_node->val and then when i am calling dfs i am subtracting current node value for the next node
 class Solution {
 public:
     void dfs(TreeNode* node,int curr_sum,vector<int>&curr_path,vector<vector<int>>&ans) {
