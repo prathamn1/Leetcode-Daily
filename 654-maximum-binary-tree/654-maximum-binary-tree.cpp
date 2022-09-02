a7c02cb300fee287a66ce443338bba96938a5826
Pratham <prathamnagaria@gmail.com>
2022-09-03 02:19:36 +0530

    Time: 139 ms (51.17%), Space: 39.3 MB (98.83%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+// not intuitive just dry run and understand the solution but for better understanding use vector as stack and traverse the whole array . now how the code will work like for every element it will check if it's greater than the top of stack and if it is then append that top element of stack to the left of that greater element and consequently pop out the stack now what will remain is either the stack will be empty or the current element is smaller than the top of stack so append that element to the right of the top of stack 
+     
+
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        vector<TreeNode*> stk;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            TreeNode* cur = new TreeNode(nums[i]);
+            while (!stk.empty() && stk.back()->val < nums[i])
+            {
+                cur->left = stk.back();
+                stk.pop_back();
+            }
+            if (!stk.empty())
+                stk.back()->right = cur;
+            stk.push_back(cur);
+        }
+        return stk.front();
+    }
+};
