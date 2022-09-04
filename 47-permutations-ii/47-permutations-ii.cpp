724f32dab276e13b471d8d8a5bca43837652ca5c
Pratham <prathamnagaria@gmail.com>
2022-09-05 04:14:53 +0530

    Time: 21 ms (43.77%), Space: 9.4 MB (52.79%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(int n,vector<int>nums,vector<int>&curr_perm,unordered_map<int,int>&visited,vector<vector<int>>&ans) {
+        if(curr_perm.size()==n) {
+            ans.push_back(curr_perm);
+        }
+        for(auto &i:visited) {
+            if(i.second) {
+                curr_perm.push_back(i.first);
+                i.second--;
+                solve(n,nums,curr_perm,visited,ans);
+                curr_perm.pop_back();
+                i.second++;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        vector<int>curr_perm;
+        unordered_map<int,int>visited;
+        for(auto i:nums) {
+            visited[i]++;
+        }
+        solve(n,nums,curr_perm,visited,ans);
+        return ans;
+    }
+};
