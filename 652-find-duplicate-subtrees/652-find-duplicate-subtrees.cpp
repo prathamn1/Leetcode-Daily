3b75d87f0a291964f19532f25bf82b42b2c2265f
Pratham <prathamnagaria@gmail.com>
2022-09-08 02:47:35 +0530

    Time: 152 ms (5.01%), Space: 64.9 MB (14.62%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -15,8 +15,10 @@ public:
         if(not root) {
             return "";
         }
+        //just make recursion tree and remember when the call goes for 4->left ie null then the function returns "" to the previous call that is when the root will be 4 and then the string will be ( after that root->val will be added so string will be (4 then again call goes for 4->right so it will return empty string and then finally a ) will be added so final string will be (4) and it will be added to the map and then the s will be returned as (4) to the previous call when the root will be 2 and so on.
+        // just make recursion tree.
         s="("+solve(root->left,ans,s,mp)+to_string(root->val)+solve(root->right,ans,s,mp)+")";
-        // cout<<s<<"  ";
+        cout<<s<<"  ";
         mp[s]++;
         if(mp[s]==2) {
             ans.push_back(root);
