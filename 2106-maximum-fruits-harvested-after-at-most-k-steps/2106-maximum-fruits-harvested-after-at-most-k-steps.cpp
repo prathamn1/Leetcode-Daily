ccbc81e265e3f00bc74cc6d18589c2d11be801f0
Pratham <prathamnagaria@gmail.com>
2022-09-11 19:08:19 +0530

    Time: 792 ms (64.01%), Space: 120.1 MB (95.86%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,3 +1,28 @@
+// Approach 1 : prefix sum;
+/*
+Just make one prefix sum array of size 2*10^5 and initialize position containing fruits as number of fruits ans others as zero and then take prefix sum of them
+for calculating the number of fruits in range [l,r] : prefix[r]-prefix[l-1] and remember that
+l-1>=0 
+Now there are two types of operations like -
+--double the steps on left and remaining steps on right 
+
+for this since you have to go double steps on left you can have remaining steps as (k-2*left) on right thus calculate the fruits on range left,right 
+
+
+--double the steps on right and remaining steps on left
+similar to above
+and calculate the maximum out of two operations;
+
+
+
+for the second approach no need to do prefix sum just use two pointer approach initially both left and right are 0 so take fruits on pos=fruits[0][1] and if the path is valid that is position of fruits on l and position of fruits on r then try to expand the range by increasing r and take the fruits on that position but if the path goes wrong ie. takes more than k steps then move l forward untill path becomes valid or l equals r.
+
+ 
+
+*/
+
+
+//second approach
 class Solution {
 public:
     bool is_valid(int l,int r,int start,int k) {
