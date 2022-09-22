fbeeea7c3acfa5cb255db4a44b2cf84a1f042a13
Pratham <prathamnagaria@gmail.com>
2022-09-23 02:43:48 +0530

    Time: 17 ms (6.06%), Space: 7.8 MB (9.73%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n=s.size();
+        string ans;
+        int l=0;
+        while(l<n) {
+            for(int i=min(l+k-1,n-1);i>=l;i--) {
+                // if(i>n-1) {
+                //     break;
+                // }
+                ans+=s[i];
+            }
+            l+=k;
+            for(int i=l;i<min(l+k,n);i++) {
+                ans+=s[i];
+            }
+            l+=k;
+            // cout<<l<<endl;
+        }
+        return ans;
+    }
+};
