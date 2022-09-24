29ed0d0a16cf5d9a02ddfc10b78aa5af8decc27d
Pratham <prathamnagaria@gmail.com>
2022-09-24 20:48:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (95.70%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         ans.push_back({});
+//         for(auto i:nums) {
+//             int length=ans.size();
+//             for(int j=0;j<length;j++) {
+//                 vector<int>temp=ans[j];
+//                 temp.push_back(i);
+//                 ans.push_back(temp);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        ans.push_back({});
-        for(auto i:nums) {
-            int length=ans.size();
-            for(int j=0;j<length;j++) {
-                vector<int>temp=ans[j];
-                temp.push_back(i);
-                ans.push_back(temp);
+        int total_subsets=1<<nums.size();
+        for(int i=0;i<total_subsets;i++) {
+            vector<int>temp;
+            for(int j=0;j<nums.size();j++) {
+                if(((i>>j)&1)==1) {
+                    temp.push_back(nums[j]);
+                }
             }
+            ans.push_back(temp);
         }
         return ans;
     }
