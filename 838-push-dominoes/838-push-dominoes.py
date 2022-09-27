f041e05cb7c3335ca5613ffd181f2f56faa0eb0c
Pratham <prathamnagaria@gmail.com>
2022-09-28 02:02:25 +0530

    Time: 616 ms (46.04%), Space: 17.2 MB (62.56%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.py
+++ 838-push-dominoes/838-push-dominoes.py
@@ -1,21 +1,24 @@
 class Solution:
     def pushDominoes(self, domino: str) -> str:
+        domino='L'+domino+'R'
         n=len(domino)
         ans=[x for x in domino]
-        i=0
-        while i<n and domino[i]=='.':
-            i+=1
-        start=i
-        if start>=0 and start<n and domino[start]=='L':
-            for j in range(i):
-                ans[j]=domino[start]
-        i=n-1
-        while i>=0 and domino[i]=='.':
-            i-=1
-        stop=i
-        if stop >=0 and stop <n and domino[stop]=='R':
-            for j in range(i+1,n):
-                ans[j]=domino[stop]
+        # i=0
+        # while i<n and domino[i]=='.':
+        #     i+=1
+        # start=i
+        # if start>=0 and start<n and domino[start]=='L':
+        #     for j in range(i):
+        #         ans[j]=domino[start]
+        # i=n-1
+        # while i>=0 and domino[i]=='.':
+        #     i-=1
+        # stop=i
+        # if stop >=0 and stop <n and domino[stop]=='R':
+        #     for j in range(i+1,n):
+        #         ans[j]=domino[stop]
+        start=0
+        stop=n-1
         while start<stop:
             right=start+1
             while right<=stop and domino[right]=='.':
@@ -43,6 +46,9 @@ class Solution:
                     l+=1
                     r-=1
                 start=right
-        return ''.join(ans)
+        return ''.join(ans[1:n-1])
                     
 # print(Solution().pushDominoes(".L.R...LR..L.."))
+
+
+        
