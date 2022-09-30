a564307b7ad63e8c9aa3c288a16d46fb920d0e9e
Pratham <prathamnagaria@gmail.com>
2022-09-30 16:11:09 +0530

    Time: 299 ms (32.61%), Space: 93.1 MB (94.86%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -6,9 +6,10 @@ public:
             if(r>0 and r<n and nums[r-1]-nums[r]!=1) {
                 l=r;
             }
-            ans+=r-l+1;
+            ans+=r-l+1; // just remember this as a method to count subarrays when right increses every time or for every iteration.
             r+=1;
         }
         return ans;
     }
 };
+//this question can also be done by using the formula for subarrays as n*(n+1)/2
