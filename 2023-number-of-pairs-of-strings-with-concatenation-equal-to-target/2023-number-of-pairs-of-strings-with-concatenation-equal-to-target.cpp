bf20606e465703559ce8fbbdac50e46eefac536e
Pratham <prathamnagaria@gmail.com>
2022-10-05 01:00:30 +0530

    Time: 26 ms (68.71%), Space: 13.5 MB (60.07%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int n=nums.size(),ans=0;
+        int num=nums.size(),ans=0,n=target.size();
+        unordered_map<string,int>mp;
+        for(auto i:nums) {
+            mp[i]++;
+        }
         
-//         unordered_map<string,int>mp;
-//         for(auto i:nums) {
-//             mp[i]++;
-//         }
-//         vector<pair<string,string>>pre_suff;
-//         string pre="",suff="";
-//         for(int i=0;i<n;i++) {
-//             pre+=target[i];
-//             suff=target[n-i-1]+suff;
-//             pre_suff.push_back({pre,suff});
-//         }
-//         for(auto i:pre_suff) {
-//             if(mp.find(i.first)!=mp.end() and mp.find(i.second)!=mp.end()) {
-//                 if(i.first==i.second) {
-//                     ans+=mp[i.first]*(mp[i.first]-1);
-//                 }else {
-//                     ans+=mp[i.first]*mp[i.second];
-//                 }
-//             }
-//         }
-//         return ans;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(i==j) {
-                    continue;
-                }
-                if(nums[i]+nums[j]==target) {
-                    ans+=1;
+        vector<pair<string,string>>pre_suff;
+        string pre=target,suff="";
+        pre_suff.push_back({pre,suff});
+        for(int i=n-1;i>=0;i--) {
+            suff=target[i]+suff;
+            pre.pop_back();
+            pre_suff.push_back({pre,suff});
+        }
+        // for(auto [str,ct]:pre_suff) {
+        //     cout<<str<<" "<<ct<<endl;
+        // }
+        for(auto i:pre_suff) {
+            if(mp.find(i.first)!=mp.end() and mp.find(i.second)!=mp.end()) {
+                if(i.first+i.first==target) {
+                    ans+=mp[i.first]*(mp[i.first]-1);
+                }else {
+                    ans+=mp[i.first]*mp[i.second];
                 }
             }
         }
         return ans;
+        // for(int i=0;i<num;i++) {
+        //     for(int j=0;j<num;j++) {
+        //         if(i==j) {
+        //             continue;
+        //         }
+        //         if(nums[i]+nums[j]==target) {
+        //             ans+=1;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
