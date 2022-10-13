16efb57a4711e4b2dce8228ec9cf48fb0a171d8f
Pratham <prathamnagaria@gmail.com>
2022-10-13 08:38:15 +0530

    Time: 41 ms (7.92%), Space: 7.6 MB (73.12%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//if we can't delete anything or can't traverse then the easiest way to solve is to copy the value of the next node to the current node and make the last node to null
 class Solution {
 public:
     void deleteNode(ListNode* node) {
