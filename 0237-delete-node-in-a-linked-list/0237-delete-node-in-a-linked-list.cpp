5f9739936453b01a2aa3cf4853731f2a57be853c
Pratham <prathamnagaria@gmail.com>
2022-10-13 08:41:12 +0530

    Time: 46 ms (5.17%), Space: 7.7 MB (37.95%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -8,15 +8,24 @@
  */
 
 //if we can't delete anything or can't traverse then the easiest way to solve is to copy the value of the next node to the current node and make the last node to null
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+//         while(node->next->next) {
+//             node->val=node->next->val;
+//             node=node->next;
+//         }
+//         node->val=node->next->val;
+//         node->next=NULL;
+        
+//     }
+// };
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        while(node->next->next) {
-            node->val=node->next->val;
-            node=node->next;
-        }
         node->val=node->next->val;
-        node->next=NULL;
+        node->next=node->next->next;
         
     }
 };
