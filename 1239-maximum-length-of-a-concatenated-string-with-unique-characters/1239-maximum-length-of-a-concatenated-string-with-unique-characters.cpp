29081280d1aafdd86770a80b60d55dbae049a264
Pratham <prathamnagaria@gmail.com>
2022-10-24 09:41:57 +0530

    Time: 1343 ms (7.96%), Space: 522.7 MB (5.05%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(int curr_index,set<char>curr_set,vector<string>&arr,int &ans) {
+        if(curr_index==arr.size()) {
+            ans=max<int>(curr_set.size(),ans);
+            return ;
+        }
+        solve(curr_index+1,curr_set,arr,ans);
+        // set<char>temp(curr_set.begin(),curr_set.end());
+        for(auto i:arr[curr_index]) {
+            if(curr_set.find(i)==curr_set.end()) {
+                curr_set.insert(i);
+            }else {
+                return ;
+            }
+        }
+        solve(curr_index+1,curr_set,arr,ans);
+        
+    }
+    int maxLength(vector<string>& arr) {
+        set<char>curr_set;
+        int ans=0;
+        solve(0,curr_set,arr,ans);
+        return ans;
+    }
+};
