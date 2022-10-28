a984a393749f00f2ed1f89c1bf66ef848202dc91
Pratham <prathamnagaria@gmail.com>
2022-10-28 17:06:31 +0530

    Time: 859 ms (59.65%), Space: 127.8 MB (78.95%) - LeetHub

---
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& nums) {
+        int n=nums.size();
+        vector<long long>ans(n);
+        unordered_map<int,vector<int>>position;
+        for(int i=0;i<n;i++) {
+            position[nums[i]].emplace_back(i);
+        }
+        for(auto [ele,pos]:position) {
+            int leng=pos.size();
+            if(leng==1) {
+                ans[pos[0]]=0;
+            }else {
+                long long int s1=0,s2=0;
+                for(int i=1;i<leng;i++) {
+                    s1+=(leng-i)*(pos[i]-pos[i-1]);
+                }
+                for(int i=0;i<leng;i++) {
+                    if(i==0) {
+                        ans[pos[i]]=s1;
+                    }else {
+                        s1-=(leng-i)*(pos[i]-pos[i-1]);
+                        s2+=i*(pos[i]-pos[i-1]);
+                        ans[pos[i]]=s1+s2;
+                    }   
+                }
+            }
+        }
+        return ans;
+    }
+};
