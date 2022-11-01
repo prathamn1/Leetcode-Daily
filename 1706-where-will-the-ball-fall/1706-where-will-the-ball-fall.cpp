c9cd04fb4cfd489e733443963aeed20fe21df6ba
Pratham <prathamnagaria@gmail.com>
2022-11-01 13:51:14 +0530

    Time: 26 ms (92.31%), Space: 13.3 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int solve(int curr_row,int curr_col,int m,int n,vector<vector<int>>&dp,vector<vector<int>>&grid) {
+    // int solve(int curr_row,int curr_col,int m,int n,vector<vector<int>>&dp,vector<vector<int>>&grid) {
+    int solve(int curr_row,int curr_col,int m,int n,int dp[100][100],vector<vector<int>>&grid) {
+
         if(curr_row==m) {
             return curr_col;
         }
@@ -20,7 +22,9 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<int>ans;
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        int dp[100][100];
+        memset(dp,0,sizeof(dp));
         for(int i=0;i<n;i++) {
             ans.push_back(solve(0,i,m,n,dp,grid));
         }
