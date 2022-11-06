25f415cc4e986e7137b73b4723cab6862dc59666
Pratham <prathamnagaria@gmail.com>
2022-11-06 15:58:53 +0530

    Time: 11 ms (36.78%), Space: 12 MB (8.62%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,3 +1,6 @@
+//if k==1 then we will see that at one time we could send the front character to the back so brute force it and find the minimum of all strings
+//if k==2 lets we have a string abcccc then we could form the strings as ab
+
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
@@ -5,18 +8,15 @@ public:
         if(k==1) {
             ans=s;
             for(int i=0;i<s.size();i++) {
-                s=s.substr(1,s.size())+s[0];
+                s=s.substr(1)+s[0];
                 if(ans>s) {
                     ans=s;
                 }
             }
         }
         else {
-            vector<char>st(s.begin(),s.end());
-            sort(st.begin(),st.end());
-            for(auto ch : st) {
-                ans+=ch;
-            }
+            sort(s.begin(),s.end());
+            return s;
         }
         return ans;
     }
