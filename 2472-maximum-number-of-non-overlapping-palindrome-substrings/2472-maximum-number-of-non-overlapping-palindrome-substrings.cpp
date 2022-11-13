6e4a4b840a6e2ae0c18ee39916bc87900d771d48
Pratham <prathamnagaria@gmail.com>
2022-11-13 13:18:15 +0530

    Time: 21 ms (77.78%), Space: 8 MB (33.33%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -15,7 +15,7 @@ public:
             string st="";
             for(int j=i;j<n;j++) {
                 st+=s[j];
-                if(st.size()>k+1) {
+                if(st.size()>k+1) {  // actually no need for checking palindromic string if it's size exceeds k+1 because if K+2 sized string becomes palindrome then automatically one of k+1 or k will be palindrome so no need to take k+2 sized string as we need to maximize the partition
                     break;
                 }
                 if(st.size()>=k and is_palindrome(st)) {
