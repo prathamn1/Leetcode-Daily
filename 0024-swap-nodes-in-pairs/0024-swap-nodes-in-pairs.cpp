635f65b9857334c7bacb469261cf370451f885bc
Pratham <prathamnagaria@gmail.com>
2022-12-03 12:58:04 +0530

    Time: 3 ms (80.22%), Space: 7.6 MB (7.02%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -14,14 +14,14 @@ public:
         if(not head or not head->next) {
             return head;
         }
-        ListNode * dummy_node=new ListNode(),*curr_node=head,*next_node;
+        ListNode * dummy_node=new ListNode(),*curr_node=head;
         ListNode* prev_node=dummy_node;
         while(curr_node and curr_node->next) {
-            next_node=curr_node->next;
+            prev_node->next=curr_node->next;
             curr_node->next=curr_node->next->next;
-            next_node->next=curr_node;
-            prev_node->next=next_node;
-            prev_node=next_node->next;
+            prev_node->next->next=curr_node;
+            
+            prev_node=curr_node;
             curr_node=curr_node->next;
         }
         return dummy_node->next;
