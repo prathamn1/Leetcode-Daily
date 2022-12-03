29c2ea3cf672e071234d13026f057c7793116645
Pratham <prathamnagaria@gmail.com>
2022-12-03 11:46:15 +0530

    Time: 49 ms (5.82%), Space: 11.5 MB (31.13%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,3 +1,50 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* reverse_list(ListNode* curr_node,int k) {
+//         ListNode* next_node,*prev_node=NULL;
+//         for(int i=0;i<k;i++) {
+//             next_node=curr_node->next;
+//             curr_node->next=prev_node;
+//             prev_node=curr_node;
+//             curr_node=next_node; 
+//         } 
+//         return prev_node;
+//     }
+//     ListNode is_k_nodes_available(ListNode* start,int k) {
+//         while(start and k!=0) {
+//             start=start->next;
+//             k-=1;
+//         }
+//         return ListNode(k,start);
+//     }
+//     ListNode* reverseKGroup(ListNode* head, int k) {
+//         ListNode dummy_head(0,NULL);
+//         ListNode* start=head,*k_head=&dummy_head;
+//         while(true) {
+//             ListNode k_node=is_k_nodes_available(start,k);
+//             if(k_node.val==0) {
+//                 k_head->next=reverse_list(start,k);
+//                 k_head=start;
+//                 start=k_node.next;
+//             }else {
+//                 k_head->next=start;
+//                 break;
+//             }
+//         }
+//         return dummy_head.next;
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,9 +57,9 @@
  */
 class Solution {
 public:
-    ListNode* reverse_list(ListNode* curr_node,int k) {
+    ListNode* reverse_list(ListNode* curr_node,ListNode* end_node) {
         ListNode* next_node,*prev_node=NULL;
-        for(int i=0;i<k;i++) {
+        while(curr_node!=end_node) {
             next_node=curr_node->next;
             curr_node->next=prev_node;
             prev_node=curr_node;
@@ -20,27 +67,16 @@ public:
         } 
         return prev_node;
     }
-    ListNode is_k_nodes_available(ListNode* start,int k) {
-        while(start and k!=0) {
-            start=start->next;
-            k-=1;
-        }
-        return ListNode(k,start);
-    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode dummy_head(0,NULL);
-        ListNode* start=head,*k_head=&dummy_head;
-        while(true) {
-            ListNode k_node=is_k_nodes_available(start,k);
-            if(k_node.val==0) {
-                k_head->next=reverse_list(start,k);
-                k_head=start;
-                start=k_node.next;
-            }else {
-                k_head->next=start;
-                break;
+        ListNode* curr_node=head;
+        for(int i=0;i<k;i++) {
+            if(not curr_node) {
+                return head;
             }
+            curr_node=curr_node->next;
         }
-        return dummy_head.next;
+        ListNode* reverse_head=reverse_list(head,curr_node);
+        head->next=reverseKGroup(curr_node,k);
+        return reverse_head;
     }
 };
