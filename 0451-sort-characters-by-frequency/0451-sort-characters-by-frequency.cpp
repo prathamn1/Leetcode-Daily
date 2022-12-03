c3ee2bd3a0888512ab2c9472461a3600679d27ff
Pratham <prathamnagaria@gmail.com>
2022-12-03 09:31:56 +0530

    Time: 38 ms (20.13%), Space: 8.5 MB (67.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    static bool cmp(pair<char,int>&a,pair<char,int>&b) {
+        return a.second<b.second;
+    }
     string frequencySort(string s) {
         unordered_map<char,int>freq;
         for(auto i:s) {
             freq[i]++;
         }
-        // priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(cmp)>pq(&cmp);
-        priority_queue<pair<int,char>,vector<pair<int,char>>>pq;
-        for(auto [i,j]:freq) {
-            pq.push({j,i});
+        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(&cmp)>pq(cmp); 
+        // priority_queue<pair<int,char>,vector<pair<int,char>>>pq;  //36ms
+        // for(auto [i,j]:freq) {
+        //     pq.push({j,i});
+        // }
+        for(auto pr:freq) {
+            pq.push(pr);
         }
         string ans="";
         while(not pq.empty()) {
-            pair<int,char>char_freq=pq.top();
+            pair<char,int>char_freq=pq.top();
             pq.pop();
-            for(int i=0;i<char_freq.first;i++) {
-                ans+=char_freq.second;
+            for(int i=0;i<char_freq.second;i++) {
+                ans+=char_freq.first;
             }
         }
         return ans;
