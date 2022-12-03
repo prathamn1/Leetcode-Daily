5b0307abcc786e4b1d78c9783a625cc76f9dc8cf
Pratham <prathamnagaria@gmail.com>
2022-12-03 12:59:04 +0530

    Time: 4 ms (54.19%), Space: 7.5 MB (92.00%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//trick is to install one dummy node or helping node at the starting and then draw 4-5 node and make algorithm on copy on how to order them
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
