f89725efd67407a535ae0a2ad17bd4a22ec778de
Pratham <prathamnagaria@gmail.com>
2022-12-03 10:18:35 +0530

    Time: 20 ms (69.24%), Space: 8.7 MB (22.25%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    static bool cmp(pair<char,int>&a,pair<char,int>&b) {
-        return a.second<b.second;
+    static bool cmp(pair<char,pair<int,int>>&a,pair<char,pair<int,int>>&b) {
+        if(a.second.first==b.second.first) {
+            return a.second.second>b.second.second;
+        }
+        return a.second.first<b.second.first;
     }
     string frequencySort(string s) {
-        unordered_map<char,int>freq;
-        for(auto i:s) {
-            freq[i]++;
+        int n=s.size();
+        unordered_map<char,pair<int,int>>freq;//map : key=char , value : pair (frequency , first_ appearance)
+        for(int i=n-1;i>=0;i--) {
+            freq[s[i]].second=i; // setting the index of character since traversing backward at last it will store the first appearance
+            freq[s[i]].first++;//incrementing the frequency
         }
-        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(&cmp)>pq(cmp); 
-        // priority_queue<pair<int,char>,vector<pair<int,char>>>pq;  //36ms
-        // for(auto [i,j]:freq) {
-        //     pq.push({j,i});
-        // }
+        priority_queue<pair<char,pair<int,int>>,vector<pair<char,pair<int,int>>>,decltype(&cmp)>pq(cmp); 
         for(auto pr:freq) {
             pq.push(pr);
         }
         string ans="";
         while(not pq.empty()) {
-            pair<char,int>char_freq=pq.top();
+           pair<char,pair<int,int>>char_freq=pq.top();
             pq.pop();
-            for(int i=0;i<char_freq.second;i++) {
+            for(int i=0;i<char_freq.second.first;i++) {
                 ans+=char_freq.first;
             }
         }
