103f2a042e9782486b1bf42fbd481cb7d7e19646
Pratham <prathamnagaria@gmail.com>
2022-12-06 01:48:39 +0530

    Time: 12 ms (29.62%), Space: 6.2 MB (68.60%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
@@ -10,17 +10,46 @@
  * };
  */
 
+// class Solution {
+// public:
+//     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+//         vector<vector<int>>ans;
+//         for(int x=1;x<=1000;x++) {
+//             for(int y=1;y<=1000;y++) {
+//                 if(customfunction.f(x,y)==z) {
+//                     ans.push_back({x,y});
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
         vector<vector<int>>ans;
         for(int x=1;x<=1000;x++) {
-            for(int y=1;y<=1000;y++) {
-                if(customfunction.f(x,y)==z) {
-                    ans.push_back({x,y});
+            int low=1,high=1000,found=-1;
+            while(low<=high) {
+                int mid=(high+low)/2;
+                int f_mid=customfunction.f(x,mid);
+                if(f_mid==z) {
+                    found=mid;
+                    break;
+                }else if(f_mid>z) {
+                    high=mid-1;
+                }else {
+                    low=mid+1;
                 }
             }
+            if(found!=-1) {
+                for(int y=found;y<=1000 and customfunction.f(x,y)==z;y++) ans.push_back({x,y});
+                for(int y=found-1;y>=0 and customfunction.f(x,y)==z;y--) ans.push_back({x,y});
+            }
         }
-        return ans;
+        return ans;   
     }
 };
